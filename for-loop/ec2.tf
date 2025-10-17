836caf141c99f1a22de6d88a8beea8239ad76c44
reddibhavya <reddireddibhavya@gmail.com>
2025-10-17 12:42:03 +0800

    terraform,

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -1,11 +1,11 @@
   resource "aws_instance" "terraform" {
     for_each = var.instances
-    for_each = toset(var.instances)
+    # for_each = toset(var.instances)
     ami = "ami-09c813fb71547fc4f"
     instance_type = each.value
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
-      Name = each.value
+      Name = each.key
       Terraform = "true"
     }
   }
--- for-loop/variables.tf
+++ for-loop/variables.tf
@@ -1,9 +1,12 @@
 variable "instances" {
-    default = ["mongodb","redis"]
+    # default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","frontend"]
+    default = {
+        mongodb = "t3.micro"
+        redis = "t3.micro"
+        mysql = "t3.small"
+    }
+
 }
-
-
-
 
 variable "zone_id" {
     default = "Z01073672KN62W8KE0BU0"
